28df5a16da6805cf97412d1fdda79bbbd9c20585
Puru80 <puru.agar99@gmail.com>
2023-03-01 19:28:00 +0530

    project running again

--- src/main/java/com/example/thehairlabrevisited/ThehairlabRevisitedApplication.java
+++ src/main/java/com/example/thehairlabrevisited/ThehairlabRevisitedApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
